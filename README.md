e6427b27ba29045fe2d2a99052986f3226d52c2b
Jason Holt <95232586+jasongholt@users.noreply.github.com>
2022-07-26 14:36:57 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# EPG VLAN
+
+This terraform module represents a common scenario where an ACI tenant requires a single 
+network segment, operating in similar fashion
+as a legacy VLAN. 
+
+
+## Diagram
+
+![diagram](https://gitlab.com/kecorbin/terraform-aci-epg-vlan/-/raw/master/assets/diagram.png)
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "tenant_name" {
+  description = "ACI tenant name"
+  type = string
+}
+
+variable "gateway_address" {
+  description = "The default gateway for the segment example 10.10.1.1/24"
+  type = string
+}
+
+variable "unicast_route" {
+  description = "Enables or disables unicast routing for the BD (yes/no)"
+  default     = "no"
+}
